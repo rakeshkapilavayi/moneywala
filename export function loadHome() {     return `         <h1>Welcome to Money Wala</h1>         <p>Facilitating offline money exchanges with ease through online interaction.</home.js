333caeca24a7f905616a0ceaaf607eec80e24cc8
rakeshkapilavayi <157085553+rakeshkapilavayi@users.noreply.github.com>
2024-12-20 11:08:24 +0530

    Create home.js

---
+++ export function loadHome() {     return `         <h1>Welcome to Money Wala</h1>         <p>Facilitating offline money exchanges with ease through online interaction.</home.js
@@ -0,0 +1,6 @@
+export function loadHome() {
+    return `
+        <h1>Welcome to Money Wala</h1>
+        <p>Facilitating offline money exchanges with ease through online interaction.</p>
+    `;
+}
