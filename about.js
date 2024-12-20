dcbd98498d16639614334554a534157d5e47321c
rakeshkapilavayi <157085553+rakeshkapilavayi@users.noreply.github.com>
2024-12-20 11:08:57 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,6 @@
+export function loadAbout() {
+    return `
+        <h1>About Money Wala</h1>
+        <p>Money Wala connects people who need to exchange money offline using an online platform.</p>
+    `;
+}
